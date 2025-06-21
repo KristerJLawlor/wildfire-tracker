15c4a8c21d022d8db09edae1663f64017b18f237
Krister Lawlor <KristerJLawlor@gmail.com>
2025-06-20 19:53:31 -0700

    Retrieve's data from EONET and displays coordinates on Google Map API

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,26 +1,43 @@
+import { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
+import LocationMarker from './LocationMarker'
+import LocationInfoBox from './LocationInfoBox'
 
-const Map = ({ center, zoom }) => {
+const NATURAL_EVENT_WILDFIRE = "wildfires";
 
-    return (
-        <div className="map">
-            <GoogleMapReact
-                bootstrapURLKeys={{ key: 'AIzaSyCQjKjYCnnfr0Y08b96337CGTFHoB5z12s' }}
-                defaultCenter={ { lat: 42.3265, lng: -122.8756 } }
-                defaultZoom={ 6 }
-                yesIWantToUseGoogleMapApiInternals = {true}
-                            >
-            </GoogleMapReact>
-        </div>
-    )
-}
-
-Map.defaultProps = {
+const defaultProps = {
     center: {
         lat: 42.3265,
         lng: -122.8756
     },
     zoom: 6
 }
+
+const Map = ({ eventData, center = defaultProps.center, zoom = defaultProps.zoom }) => {
+    const [locationInfo, setLocationInfo] = useState(null)
+
+    const markers = eventData.map((ev, index) => {
+
+        if(ev.categories[0].id === NATURAL_EVENT_WILDFIRE) {
+            return <LocationMarker lat={ev.geometry[0].coordinates[1]} lng={ev.geometry[0].coordinates[0]} onClick={() => setLocationInfo({ id: ev.id, title: ev.title })}/>
+        }
+        return null
+    })
+
+    return (
+        <div className="map">
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyCQjKjYCnnfr0Y08b96337CGTFHoB5z12s' }}
+                defaultCenter={ center }
+                defaultZoom={ zoom }
+            >
+                {markers}
+            </GoogleMapReact>
+            {locationInfo && <LocationInfoBox info={locationInfo} />}
+        </div>
+    )
+}
+
+
 
 export default Map
